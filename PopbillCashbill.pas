53738e0a30055832b56965b6c703a12f2a3d28b3
JohnJeong123 <frenchofkiss@gmail.com>
2017-03-08 16:32:03 +0900

    fixed CheckMgtKeyInUse Exception code

--- PopbillCashbill.pas
+++ PopbillCashbill.pas
@@ -4,24 +4,13 @@
 * RESTful web service request and parse json result. It uses Linkhub module
 * to accomplish authentication APIs.
 *
-* This module uses synapse library.( http://www.ararat.cz/synapse/doku.php/ )
-* It's full open source library, free to use include commercial application.
-* If you wish to donate that, visit their site.
-* So, before using this module, you need to install synapse by user self.
-* You can refer their site or detailed infomation about installation is available
-* from below our site. We appreciate your visiting.
-*
 * For strongly secured communications, this module uses SSL/TLS with OpenSSL.
-* So You need two dlls (libeay32.dll and ssleay32.dll) from OpenSSL. You can
-* get it from Fulgan. ( http://indy.fulgan.com/SSL/ ) We recommend i386_win32 version.
-* And also, dlls must be released with your executions. That's the drawback of this
-* module, but we acommplished higher security level against that.
 *
 * http://www.popbill.com
 * Author : Kim Seongjun (pallet027@gmail.com)
 * Written : 2014-03-22
 * Contributor : Jeong Yohan (code@linkhub.co.kr)
-* Updated : 2017-02-22
+* Updated : 2017-03-08
 * Thanks for your interest. 
 *=================================================================================
 *)
@@ -372,7 +361,7 @@ begin
                                 result := false;
                                 Exit;
                         end;
-                        Raise E;
+                        raise EPopbillException.Create(-99999999,E.message);
                 end;
         end;
         cashbillInfo := jsonToTCashbillInfo(responseJson);
